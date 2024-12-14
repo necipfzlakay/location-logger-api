28c355606092c6937b606503eb511a38a3b6d5b4
Necip Fazıl Akay <akaynecip@gmail.com>
2024-12-14 18:34:42 +0300

    refactor user module: rename create method, add TypeORM integration, and implement user creation logic

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectRepository(User)
+    private readonly usersRepository: Repository<User>,
+  ) {}
+  async createUser(createUserDto: CreateUserDto) {
+    this.usersRepository.findOneBy({ username: createUserDto.username });
+    const newUser = this.usersRepository.create(createUserDto);
+    return await this.usersRepository.save(newUser);
   }
 
   findAll() {
