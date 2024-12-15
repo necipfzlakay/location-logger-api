6d7a5f2a55bbe3cc85e5c9a796cdbebe9e7a4c8c
Necip Fazıl Akay <akaynecip@gmail.com>
2024-12-15 18:46:19 +0300

    feat(areas): add documentation comments for area creation and retrieval methods

--- src/areas/areas.controller.ts
+++ src/areas/areas.controller.ts
@@ -6,16 +6,26 @@ import { CreateAreaDto } from './dto/create-area.dto';
 export class AreasController {
   constructor(private readonly areasService: AreasService) {}
 
+  /**
+   *  Creates an area with given coordinates
+   *  @param createAreaDto
+   * */
   @Post()
   create(@Body() createAreaDto: CreateAreaDto) {
     return this.areasService.create(createAreaDto);
   }
 
+  /**
+   * Returns all areas in the database
+   * */
   @Get()
   findAll() {
     return this.areasService.findAll();
   }
 
+  /**
+   * Creates custom locations for testing purposes
+   * */
   @Get('custom')
   addCustomLocations() {
     return this.areasService.addCustomLocations();
