cd763a9a537a0ec4daff84770d91a11fd1de0a94
Necip Fazıl Akay <akaynecip@gmail.com>
2024-12-15 03:01:06 +0300

    feat(locations): update LocationsController to handle user location retrieval and modify module imports

--- src/locations/locations.module.ts
+++ src/locations/locations.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { LocationsService } from './locations.service';
 import { LocationsController } from './locations.controller';
+import { UsersModule } from 'src/users/users.module';
+import { LogsModule } from 'src/logs/logs.module';
+import { AreasModule } from 'src/areas/areas.module';
 
 @Module({
+  imports: [UsersModule, LogsModule, AreasModule],
   controllers: [LocationsController],
   providers: [LocationsService],
 })
