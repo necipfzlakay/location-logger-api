ac55f6b8b9e52b93c454e6b4899085fcfab23dff
Necip Fazıl Akay <akaynecip@gmail.com>
2024-12-15 18:44:23 +0300

    refactor(users): remove longitude and latitude fields from CreateUserDto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,4 +1,4 @@
-import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateUserDto {
   // @IsString({ message: 'id must be a string' })
@@ -8,16 +8,4 @@ export class CreateUserDto {
   @IsString({ message: 'username must be a string' })
   @IsNotEmpty({ message: 'username is required' })
   username: string;
-
-  @IsNumber(
-    { allowNaN: false, allowInfinity: false },
-    { message: 'longitude must be a number' },
-  )
-  long?: number;
-
-  @IsNumber(
-    { allowNaN: false, allowInfinity: false },
-    { message: 'latitude must be a number' },
-  )
-  lat?: number;
 }
