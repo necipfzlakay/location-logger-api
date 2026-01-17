8390ef28fcf5acac29d9a59880fcb35d03dd680a
necipfzlakay <akaynecip@gmail.com>
2026-01-17 20:03:26 +0300

    feat: Add initial Areas module's CRUD functionality and module structure.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AreasModule } from './areas/areas.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    AreasModule,
 
   ],
   controllers: [AppController],
