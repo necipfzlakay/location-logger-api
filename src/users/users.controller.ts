8af0f4a854a3341872447e6ce280dfa3a70b7151
necipfzlakay <akaynecip@gmail.com>
2026-01-17 23:31:03 +0300

    feat: Implement user entity and CRUD operations, and add a Locust performance testing script.

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(private readonly usersService: UsersService) { }
+
+  /**
+   * Create a new user
+   * @param createUserDto
+   *  */
+  @Post('create')
+  createUser(@Body() createUserDto: CreateUserDto) {
+    return this.usersService.createUser(createUserDto);
+  }
+
+  /**
+   * Get all users
+   * */
+  @Get()
+  findAll() {
+    return this.usersService.findAll();
+  }
+
+  /**
+   * Get a user by id
+   * @param id
+   * */
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.usersService.findOne(id);
+  }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,68 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private readonly usersRepository: Repository<User>,
+  ) { }
+
+  /**
+   * Create a new user
+   * @param createUserDto
+   */
+  async createUser(createUserDto: CreateUserDto) {
+    try {
+      const userByUsername = await this.findByUsername(createUserDto.username);
+      if (userByUsername) throw new Error('User already exists');
+      const newUser = this.usersRepository.create(createUserDto);
+      return await this.usersRepository.save(newUser);
+    } catch (error) {
+      throw new BadRequestException(error.message);
+    }
+  }
+
+  /**
+   *  Find a user by id
+   * @param id
+   */
+  async findById(id: string) {
+    const user = await this.usersRepository.findOneBy({ id });
+    return user;
+  }
+
+  /**
+   *  Find a user by username
+   * @param username
+   */
+  async findByUsername(username: string) {
+    const user = await this.usersRepository.findOneBy({ username });
+    return user;
+  }
+
+  /**
+   *  Get all users
+   */
+  async findAll() {
+    try {
+      return await this.usersRepository.find();
+    } catch (error) {
+      throw new BadRequestException(error.message);
+    }
+  }
+  /**
+   *  Get a user by id
+   * @param id
+   */
+  async findOne(id: string) {
+    try {
+      return await this.findById(id);
+    } catch (error) {
+      throw new BadRequestException(error.message);
+    }
+  }
+}
