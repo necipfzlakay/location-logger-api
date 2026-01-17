bfc10b5d191ecd6ec1c9d241f86033d8922ac5da
necipfzlakay <akaynecip@gmail.com>
2026-01-17 19:49:37 +0300

    feat: Integrate dedicated config and database NestJS libraries, including global validation and project configuration.

---
+++ libs/database/src/database.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (ConfigService: ConfigService) => ({
+        type: 'postgres',
+        host: ConfigService.get('DB_HOST'),
+        port: ConfigService.get('DB_PORT'),
+        username: ConfigService.get('DB_USERNAME'),
+        password: ConfigService.get('DB_PASSWORD'),
+        // database: ConfigService.get('DB_NAME'),
+        entities: [join(process.cwd(), 'dist/**/*.entity.js')],
+        synchronize: true, //! Don't use this in production
+        logging: true,
+      }),
+    }),
+  ],
+})
+export class DatabaseModule { }
