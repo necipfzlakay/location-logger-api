b1be2337f75372622e717bfe5b1475d98edaa2f5
necipfzlakay <akaynecip@gmail.com>
2026-01-17 20:00:51 +0300

    feat: Implement global configuration and DTO validation, update the default listening port, and configure Jest module mapping for the database library.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  // //! if we want to add a prefix to all routes for versioning
+  // app.setGlobalPrefix('v1');
+  app.useGlobalPipes(new ValidationPipe({}));
+  await app.listen(process.env.PORT ?? 3333);
 }
 bootstrap();
