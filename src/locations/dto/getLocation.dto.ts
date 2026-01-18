34a079d48c81083d594ac187b73e83a5eab61304
necipfzlakay <akaynecip@gmail.com>
2026-01-18 11:08:59 +0300

    feat: add GetUsersLocationDto for validating user location retrieval parameters.

---
+++ src/locations/dto/getLocation.dto.ts
@@ -0,0 +1,13 @@
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+export class GetUsersLocationDto {
+  @IsString({ message: 'user_id must be a string' })
+  @IsNotEmpty({ message: 'user_id is required' })
+  user_id: string;
+
+  // @IsNotEmpty({ message: 'long is required' })
+  long?: number;
+
+  // @IsNotEmpty({ message: 'lat is required' })
+  lat?: number;
+}
