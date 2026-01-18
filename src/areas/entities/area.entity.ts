9a2377407d8c6967e54802757e4dc9ddd1b3a598
necipfzlakay <akaynecip@gmail.com>
2026-01-18 11:02:30 +0300

    area entity created

---
+++ src/areas/entities/area.entity.ts
@@ -0,0 +1,21 @@
+
+import { Logs } from 'src/logs/entities/logs.entitiy';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'areas' })
+export class Areas {
+  // primary id of areas
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'jsonb', nullable: true })
+  polygon: Array<[]>;
+
+  // name of area (optional)
+  @Column({ nullable: true })
+  name: string;
+
+  // Relationship with Logs entity
+  @OneToMany(() => Logs, (logs) => logs.area_id)
+  logs: Logs;
+}
