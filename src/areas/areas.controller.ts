fa72d666ff6750e3f32f43ea28a6980992b41ade
necipfzlakay <akaynecip@gmail.com>
2026-01-19 22:39:54 +0300

    add: create area swagger implementation

--- src/areas/areas.controller.ts
+++ src/areas/areas.controller.ts
@@ -1,7 +1,9 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
+import { ApiBody, ApiTags } from '@nestjs/swagger';
 import { AreasService } from './areas.service';
 import { CreateAreaDto } from './dto/create-area.dto';
 
+@ApiTags('areas')
 @Controller('areas')
 export class AreasController {
   constructor(private readonly areasService: AreasService) { }
@@ -10,6 +12,44 @@ export class AreasController {
    *  Creates an area with given coordinates
    *  @param createAreaDto
    * */
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        name: { type: 'string', example: 'Test Area' },
+        polygon: {
+          type: 'array',
+          items: {
+            type: 'array',
+            items: { type: 'number' }
+          },
+          example: [
+            [28.973, 41.011],
+            [28.980, 41.011],
+            [28.980, 41.005],
+            [28.973, 41.005],
+            [28.973, 41.011]
+          ]
+        }
+      }
+    },
+    examples: {
+      'Test Area': {
+        summary: 'A simple rectangular area',
+        description: 'A small rectangular area somewhere in Istanbul',
+        value: {
+          name: 'Test Area',
+          polygon: [
+            [28.973, 41.011],
+            [28.980, 41.011],
+            [28.980, 41.005],
+            [28.973, 41.005],
+            [28.973, 41.011]
+          ]
+        }
+      }
+    }
+  })
   @Post()
   create(@Body() createAreaDto: CreateAreaDto) {
     return this.areasService.create(createAreaDto);
--- src/areas/dto/create-area.dto.ts
+++ src/areas/dto/create-area.dto.ts
@@ -1,9 +1,30 @@
-import { IsArray, IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsArray, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateAreaDto {
-  name?: string;
+  @ApiProperty({ example: 'Central Park', description: 'Name of the area' })
+  @IsString()
+  @IsNotEmpty()
+  name: string;
 
   // polygon?: number[][];
+  @ApiProperty({
+    example: [
+      [28.973, 41.011],
+      [28.980, 41.011],
+      [28.980, 41.005],
+      [28.973, 41.005],
+      [28.973, 41.011]
+    ],
+    description: 'Array of coordinates [longitude, latitude] defining the polygon',
+    type: 'array',
+    items: {
+      type: 'array',
+      items: {
+        type: 'number'
+      }
+    }
+  })
   @IsNotEmpty({ message: 'polygon is required' })
   @IsArray({ message: 'polygon must be an array' })
   polygon: Array<number[]>
